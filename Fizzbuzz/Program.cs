a38bcbb7da9029f74434932b24efb65a2adcfdc0
Hugh Hudson <Hugh.Hudson@softwire.com>
2021-11-17 12:27:14 +0000

    initial setup

---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+
+        static string IsMultiple(int number, int divider, string message)
+        {
+            string nothing = string.Empty; 
+            
+            if (number % divider == 0)
+            {
+                return message;
+            }
+            else
+            {
+                return nothing;
+            }
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(IsMultiple(i,3,"test"));
+            }
+        }
+    }
+}
